967f275b53f9a1836e1c634f82b78b6597dc5cfb
Ping Zhao <pzhao11@gmail.com>
2020-04-20 20:50:16 -0700

    Finished json import

---
+++ CafeJson-Ping-Zhao/Cafe.swift
@@ -0,0 +1,15 @@
+//
+//  Cafe.swift
+//  CafeJson-Ping-Zhao
+//
+//  Created by Ping Zhao on 4/20/20.
+//  Copyright © 2020 Ping Zhao. All rights reserved.
+//
+
+import Foundation
+
+struct Cafe: Decodable {
+    let BUSNAME: String
+    let STREET: String
+    let CITY: String
+}
--- CafeJson-Ping-Zhao/ViewController.swift
+++ CafeJson-Ping-Zhao/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cafes = [Cafe]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadData()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    func loadData() {
+        let path = Bundle.main.path(forResource: "cafes_sf", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        
+        do{
+            let data = try Data(contentsOf: url)
+            self.cafes = try JSONDecoder().decode([Cafe].self, from: data)
+            
+            for eachCafe in self.cafes {
+                print(eachCafe.BUSNAME)
+            }
+        }
+        catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cafes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = cafes[indexPath.row].BUSNAME
+        cell.detailTextLabel?.text = cafes[indexPath.row].STREET
+        
+        return cell
+    }
+    
+    
+}
