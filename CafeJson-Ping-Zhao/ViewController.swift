537644cf880cfb0ca01dc5ed7d519f0690a67fe6
Ping Zhao <pzhao11@gmail.com>
2020-04-20 20:01:33 -0700

    Initial Commit

---
+++ CafeJson-Ping-Zhao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CafeJson-Ping-Zhao
+//
+//  Created by Ping Zhao on 4/20/20.
+//  Copyright © 2020 Ping Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
